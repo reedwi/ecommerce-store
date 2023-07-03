625e45e2494092682017a8ca7ba0fef1fa7a27a1
reedwi <riandolo103@gmail.com>
2023-07-03 16:42:16 -0400

    dont get sued

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          &copy; 2023 Reed. All rights reserved
+          &copy; 2023 Reed Iandolo. All rights reserved
         </p>
       </div>
     </footer>
